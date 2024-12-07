af9ab57a732c5a58b2241af647369a312bfa304c
anjila1210 <anjilashreshth19@gmail.com>
2024-12-07 22:17:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import SideBar from './Components/SideBar/SideBar'
+import Mainy from './Components/Mainy/Mainy'
+
+
+const App = () => {
+  return (
+    <>
+    <SideBar/>
+    <Mainy/>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import ContextProvider from './Context/context.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+  <ContextProvider>
+    <App/>
+  </ContextProvider>,
+
+)
+
+
+
+  
+
